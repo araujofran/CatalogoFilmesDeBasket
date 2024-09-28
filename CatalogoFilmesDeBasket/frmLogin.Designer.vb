9be1c767e78400dbde9f0dc61493576bd8b1b3ab
fferr <fferr@DESKTOP-5MV4H0C>
2024-09-27 21:33:17 -0300

    Adicionar arquivos de projeto.

---
+++ CatalogoFilmesDeBasket/frmLogin.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.btn_entrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_usuario = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btn_fechar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_entrar
+        '
+        Me.btn_entrar.BackColor = System.Drawing.Color.Transparent
+        Me.btn_entrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_entrar.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btn_entrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btn_entrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_entrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_entrar.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn_entrar.Location = New System.Drawing.Point(29, 389)
+        Me.btn_entrar.Name = "btn_entrar"
+        Me.btn_entrar.Size = New System.Drawing.Size(83, 30)
+        Me.btn_entrar.TabIndex = 0
+        Me.btn_entrar.Text = "Entrar"
+        Me.btn_entrar.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(25, 282)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Usuário"
+        '
+        'txt_usuario
+        '
+        Me.txt_usuario.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.txt_usuario.Location = New System.Drawing.Point(102, 282)
+        Me.txt_usuario.Name = "txt_usuario"
+        Me.txt_usuario.Size = New System.Drawing.Size(162, 20)
+        Me.txt_usuario.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(25, 322)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Senha"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.TextBox1.Location = New System.Drawing.Point(102, 322)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox1.Size = New System.Drawing.Size(162, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'btn_fechar
+        '
+        Me.btn_fechar.BackColor = System.Drawing.Color.Transparent
+        Me.btn_fechar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_fechar.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn_fechar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btn_fechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_fechar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_fechar.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn_fechar.Location = New System.Drawing.Point(743, 12)
+        Me.btn_fechar.Name = "btn_fechar"
+        Me.btn_fechar.Size = New System.Drawing.Size(45, 33)
+        Me.btn_fechar.TabIndex = 6
+        Me.btn_fechar.Text = "X"
+        Me.btn_fechar.UseVisualStyleBackColor = False
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn_fechar)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_usuario)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_entrar)
+        Me.Name = "frmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_entrar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_usuario As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn_fechar As Button
+End Class
---
+++ CatalogoFilmesDeBasket/frmLogin.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmLogin
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_entrar.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+End Class
